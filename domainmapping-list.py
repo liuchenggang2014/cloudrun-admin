255028b23f39e93f0de102338f3e3bcc6da2b443
chenggang liu@g <liuchenggang@google.com>
2022-01-10 03:38:48 +0000

    first commit

---
+++ domainmapping-list.py
@@ -0,0 +1,26 @@
+import googleapiclient.discovery
+import httplib2
+import pprint
+
+
+
+
+def main():
+    # Application default credentials are provided in Google API Client Libraries automatically.
+    # You just have to build and initialize the API:
+
+    # service = googleapiclient.discovery.build('run', 'v1', http=None, discoveryServiceUrl="https://us-central1-run.googleapis.com")
+    service = googleapiclient.discovery.build('run', 'v1')
+
+    try:
+        lists = service.namespaces().domainmappings().list(parent="namespaces/cliu201").execute()
+        pprint.pprint(lists)
+        pprint.pprint('**************************************')
+
+    except httplib2.HttpLib2Error as e:
+        print('Error response status code : {0}, reason : {1}'.format(
+            e.status_code, e.error_details))
+
+
+if __name__ == '__main__':
+    main()
